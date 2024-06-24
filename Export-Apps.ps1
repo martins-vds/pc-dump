5c25883d37dc237cabc1c9dc60d2b9494e72f6ff
Vinny Martins <martins.vds@outlook.com>
2024-06-24 11:06:16 -0600

    Refactor Export-Apps.ps1 script to remove unnecessary code

--- Export-Apps.ps1
+++ Export-Apps.ps1
@@ -1,10 +1,5 @@
 $ErrorActionPreference = 'Stop'
 
-# Check if the specified folder exists
-if (-not (Test-Path $Path -PathType Container)) {
-    New-Item -Path $Path -ItemType Directory | Out-Null
-}
-
 Write-Host "Exporting installed applications to apps.json"
 
 winget.exe export --accept-source-agreements -o "apps.json" | Out-Null
